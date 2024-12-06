832f976cb23e03f7232348a6403430d1746c29c9
kssred <hi@ksred.ru>
2024-12-06 20:39:34 +0300

    Обновить конфиг nvim

--- lua/community.lua
+++ lua/community.lua
@@ -3,9 +3,9 @@ return {
   "AstroNvim/astrocommunity",
 
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.python-ruff" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.ansible" },
+  { import = "astrocommunity.pack.sql" },
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,7 +7,6 @@ return {
     config.sources = {
       -- Set a formatter
       null_ls.builtins.formatting.stylua,
-      null_ls.builtins.formatting.black,
     }
     return config -- return final config table
   end,
